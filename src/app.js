6679f4c02cfeb5ed7353a480f354e979610aeafd
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-07-08 21:33:13 -0300

    Add basic component structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Filter from './filter';
+import Table from './table';
+import fetch from './fetch';
 
 const App = () => {
+  const [listado, setListado] = useState([]);
+  useEffect(() => {
+    const getData = async () =>{
+      const response = await fetch();
+      const results = await response.json();
+      console.log(results);
+    };
+    getData();
+  }, []);
+
   return (
-  "APP"
+    <>
+      <Filter />
+      <Table />
+    </>
   )
 }
 
---
+++ src/table.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Table_ = () => {
+  return (
+  "Table_"
+  )
+}
+
+export default Table_;
