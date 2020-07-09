4f6d0861379ee4aa31b117be7127cce67c5c3f5a
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-07-08 23:26:41 -0300

    Generate mock data on fetch without .env

--- src/fetch.js
+++ src/fetch.js
@@ -1,11 +1,39 @@
+import moment from 'moment';
 
 var myHeaders = new Headers();
 if(process.env.API_KEY)
   myHeaders.append("x-apikey", process.env.API_KEY);
 
+
+const generateAmount = (order=3)=>Math.round(Math.random() * Math.pow(10, order) *100)/100;
+const MOCK = {
+  json: () => ({
+    ['data to show']: [... new Array(50)].map((x, id)=>({
+      ['ID']: '_ID_'+id,
+      ['Comercio']: '_Comercio_'+id,
+      ['CUIT']: '_CUIT_',
+      ['Concepto 1']: generateAmount(),
+      ['Concepto 2']: generateAmount(),
+      ['Concepto 3']: generateAmount(),
+      ['Concepto 4']: generateAmount(),
+      ['Concepto 5']: generateAmount(),
+      ['Concepto 6']: generateAmount(),
+      ['Balance actual']: generateAmount(4),
+      ['Activo']: Math.random()>.5,
+      ['Ultima venta']: moment().subtract(Math.round(Math.random()*100), 'days').format('DD/MM/YYYY'),
+    })),
+    ['rows total']: 50,
+    ['rows per page']: 3,
+    ['sort']: [],
+  })
+}
+
 const component = (query='', options)=> {
+  console.log(`%c >>> ${query}`, 'color: green; background: black; font-weight:bold; font-size: 14px;');
+  if (!process.env.API_ENDPOINT)
+    return MOCK
   const url = process.env.API_ENDPOINT + query;
-  console.log(`%c >>> ${url}`, 'color: green; background: black; font-weight:bold; font-size: 14px;');
   return fetch(url , {headers: myHeaders, ...options})
 }
+
 export default component;
