187da129209410fbf42ef6bc25271063c258a990
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-07-08 23:28:33 -0300

    Layout table component

--- src/table.js
+++ src/table.js
@@ -1,8 +1,118 @@
 import React from 'react';
 
-const Table_ = () => {
+import { useSelector, useDispatch } from 'react-redux';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
+import Paper from '@material-ui/core/Paper';
+
+const Table_ = ({data=[]}) => {
+  const result = useSelector(state=>state.table.result);
+  const dispatch = useDispatch();
+  console.log(result)
+  const rows = result['data to show'] || [];
   return (
-  "Table_"
+  <Paper elevation={3}>
+    <TableContainer>
+      <Table size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              ID
+            </TableCell>
+            <TableCell>
+              Comercio
+            </TableCell>
+            <TableCell>
+              CUIT
+            </TableCell>
+            <TableCell>
+              Concepto 1
+            </TableCell>
+            <TableCell>
+              Concepto 2
+            </TableCell>
+            <TableCell>
+              Concepto 3
+            </TableCell>
+            <TableCell>
+              Concepto 4
+            </TableCell>
+            <TableCell>
+              Concepto 5
+            </TableCell>
+            <TableCell>
+              Concepto 6
+            </TableCell>
+            <TableCell>
+              Balance actual
+            </TableCell>
+            <TableCell>
+              Activo
+            </TableCell>
+            <TableCell>
+              Ultima venta
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map( row => (
+            <TableRow key={row['ID']}>
+              <TableCell>
+                {row['ID']}
+              </TableCell>
+              <TableCell>
+                {row['Comercio']}
+              </TableCell>
+              <TableCell>
+                {row['CUIT']}
+              </TableCell>
+              <TableCell>
+                {row['Concepto 1']}
+              </TableCell>
+              <TableCell>
+                {row['Concepto 2']}
+              </TableCell>
+              <TableCell>
+                {row['Concepto 3']}
+              </TableCell>
+              <TableCell>
+                {row['Concepto 4']}
+              </TableCell>
+              <TableCell>
+                {row['Concepto 5']}
+              </TableCell>
+              <TableCell>
+                {row['Concepto 6']}
+              </TableCell>
+              <TableCell>
+                {row['Balance actual']}
+              </TableCell>
+              <TableCell>
+                {row['Activo']?"✅":"❌"}
+              </TableCell>
+              <TableCell>
+                {row['Ultima venta']}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    <TablePagination
+      rowsPerPageOptions={[]}
+      component="div"
+      count={result["rows total"] || -1}
+      rowsPerPage={result["rows per page"] || 10}
+      page={0}
+      onChangePage={console.log}
+    />
+  </Paper>
   )
 }
 
