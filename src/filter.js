385af72dc81b933d2caefd86c6b738f599005733
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-07-15 20:38:08 -0300

    Adds memoized callbacks

--- src/filter.js
+++ src/filter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 import {
@@ -21,6 +21,28 @@ import Paper from '@material-ui/core/Paper';
 const Filter = () => {
   const dispatch = useDispatch();
   const {id, CUIT, name, active, inactive} = useSelector(state => state.table);
+  const [
+    onSet_id,
+    onSet_CUIT,
+    onSet_name,
+    onSet_active,
+    onSet_inactive,
+    onGetFilteredData
+  ] = useMemo(()=>[
+    e => dispatch(set_id(e.target.value)),
+    e => dispatch(set_CUIT(e.target.value)),
+    e => dispatch(set_name(e.target.value)),
+    (e, val) => dispatch(set_active(val)),
+    (e, val) => dispatch(set_inactive(val)),
+    () => dispatch(getFilteredData()),
+  ],[
+    set_id,
+    set_CUIT,
+    set_name,
+    set_active,
+    set_inactive,
+    getFilteredData,
+  ]);
 
   return (
     <div>
@@ -28,24 +50,24 @@ const Filter = () => {
         <FormLabel component="legend" style={{paddingBottom: 20}}>Filtros</FormLabel>
         <Paper elevation={3} style={{padding: 20}}>
           <div style={{display:'flex', flexDirection:'row', alignItems:'center', paddingBottom:20}}>
-            <TextField id="id" label="id" value={id} onChange={e => dispatch(set_id(event.target.value))}/>
-            <TextField id="CUIT" label="CUIT" value={CUIT} onChange={e => dispatch(set_CUIT(event.target.value))}/>
-            <TextField id="name" label="Comercio" value={name} onChange={e => dispatch(set_name(event.target.value))}/>
+            <TextField id="id" label="id" value={id} onChange={onSet_id}/>
+            <TextField id="CUIT" label="CUIT" value={CUIT} onChange={onSet_CUIT}/>
+            <TextField id="name" label="Comercio" value={name} onChange={onSet_name}/>
             <div style={{marginLeft:40}}>
               <FormControlLabel
                 control={
-                  <Checkbox checked={active} onChange={(e, val) => dispatch(set_active(val))} name="active" />
+                  <Checkbox checked={active} onChange={onSet_active} name="active" />
                 }
                 label="Solo activos"
               />
               <FormControlLabel
                 control={
-                  <Checkbox checked={inactive} onChange={(e, val) => dispatch(set_inactive(val))} name="inactive" />
+                  <Checkbox checked={inactive} onChange={onSet_inactive} name="inactive" />
                 }
                 label="Solo inactivos"
               />
             </div>
-            <Button variant='contained' color='primary' onClick={() => dispatch(getFilteredData())}>Filtrar</Button>
+            <Button variant='contained' color='primary' onClick={onGetFilteredData}>Filtrar</Button>
           </div>
         </Paper>
       </FormControl>
